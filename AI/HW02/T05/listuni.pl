429c0b64ea18be9fcf152931560ab578bd20fd51
Kaitlin Hipkin <kah5368@rit.edu>
2016-04-22 01:46:51 +0200

    AI/HW02/T05 #6 was wrong, make spacing consistent, add detailed comments

--- AI/HW02/T05/listuni.pl
+++ AI/HW02/T05/listuni.pl
@@ -6,7 +6,6 @@
 % Example:
 % 1.  [A,B,C]        -- [jerry,likes,fish]
 %   A=jerry, B=likes, C=fish
-
 test_unify(1,[A,B,C],[jerry,likes,fish]) :-
   A = jerry,
   B = likes,
@@ -18,41 +17,52 @@ test_unify(1,[A,B,C],[jerry,likes,fish]) :-
 test_unify(2, [a|b] ,[a,b]) :- fail.
 
 % 3.  [cat]         -- [X|Y]
+% 3.  [cat | []]    -- [X | Y]
+%   X=cat, Y=[]
 test_unify(3, [cat], [X|Y]) :-
     X = cat,
     Y = [].
+
 % 4.  [A,B|C]       -- [mary,[does,not,like],fish]
+% 4.  [A, B | C]    -- [mary, [does,not,like] | [fish]]
+%   A=mary, B=[does,not,like], C=[fish]
 test_unify(4, [A,B|C], [mary,[does,not,like],fish]) :-
-    A=mary,
-    B=[does, not, like],
-    C=[fish].
-% 5.  [['I'|A]|B]   -- [[I,go],[by,bike]]
+    A = mary,
+    B = [does,not,like],
+    C = [fish].
+
+% 5.  [['I'|A]|B]       -- [[I,go],[by,bike]]
+% 5.  [['I' | A] | B]   -- [[I | [go]] | [[by,bike]]]
+%   I='I', A=[go], B=[[by,bike]]
 test_unify(5, [['I'|A]|B], [[I,go],[by,bike]]) :-
+    I = 'I',
     A = [go],
-    B = [[by, bike]],
-    I = 'I'.
+    B = [[by,bike]].
 
 % 6.  [nice|A]      -- [nice,weekend]
-test_unify(6, [nice|A], [nice,weekend] ) :-
-    A = weekend.
+% 6.  [nice | A]    -- [nice | [weekend]]
+%   A=[weekend]
+test_unify(6, [nice|A], [nice,weekend]) :-
+    A = [weekend].
 
 % 7.  [lame,duck]   -- [duck,A]
-test_unify(7,[lame,duck], [duck,A] ) :- fail.
+%   fail: lame =/= duck
+test_unify(7, [lame,duck], [duck,A]) :- fail.
 
 % 8.  [lame|P]      -- [x|duck]
+%   fail: lame =/= x
 test_unify(8, [lame|P], [x|duck]) :- fail.
 
 % 9. [Lame,Duck]    -- [lahme,ente]
+%   Lame=lahme, Duck=ente
 test_unify(9, [Lame,Duck], [lahme,ente]) :-
     Lame = lahme,
     Duck = ente.
 
-% 10. [[[a,b],c],d] -- [A,B|C]
+% 10. [[[a,b],c],d]         -- [A,B|C]
+% 10. [[[a,b],c], d | []]   -- [A, B | C]
+%   A=[[a,b],c], B=d, C=[]
 test_unify(10, [[[a,b],c],d], [A,B|C]) :-
-    A = [[a, b], c],
+    A = [[a,b],c],
     B = d,
     C = [].
-/*
-Hint:
-use the Prolog system to verify your tries
-*/
