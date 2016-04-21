936c59ca9df09f4771b02eab93b8dae953ca4c5b
Kaitlin Hipkin <kah5368@rit.edu>
2016-04-21 20:26:07 +0200

    AI/HW02/T02 replace old answer file with alt answer file

--- AI/HW02/T02/classschedule.pl
+++ AI/HW02/T02/classschedule.pl
@@ -4,7 +4,6 @@
 %
 % Author: T. Wilgenbusch, K. Hipkin, T. Fairman, B. Dell
 
-
 /*------------------------------------------------------------
 Implement the following class schedule:
 --------------------------------------------------------------
@@ -49,17 +48,56 @@ Example:
 lecture_db(lecture('Multi-Agent Systems', 'Kühnberger', time_room('Thu 16-18','31/147'))).
 .....
 
-If there are multible values for a field (e.g. for times and rooms, or teachers)
+If there are multiple values for a field (e.g. for times and rooms, or teachers)
 use a construction and(first_value, second_value)
 
---------------------------------------------------------------
+--------------------------------------------------------------*/
+
+lecture_db(lecture(
+  'Multi-Agent Systems',
+  'Kühnberger',
+  time_room('Thu 16-18', '31/147'))).
+lecture_db(lecture(
+  'Functional Programming',
+  'Gust',
+  time_room('Thu 12-14', '69/E23'))).
+lecture_db(lecture(
+  'Aspects of knowledge representation in artificial general intelligence',
+  'Kühnberger',
+  time_room('Tue 12-14', '31/147'))).
+lecture_db(lecture(
+  'Entscheidungs- und Spieltheorie für Studierende der Kognitionswissenschaft',
+  'Gaertner',
+  time_room('Thu 14-16', '31/423'))).
+lecture_db(lecture(
+  'Modal Logic',
+  'Martinez Baldares',
+  time_room('Tue 14-16', '31/449'))).
+lecture_db(lecture(
+  'Introduction to Algebraic Logic',
+  and('Martinez Baldares', 'Kühnberger'),
+  time_room('Mon 14-16', '31/423'))).
+lecture_db(lecture(
+  'Artificial Intelligence',
+  and('Gust', 'Kühnberger'),
+  and(time_room('Mon 14-16', '31/449'), time_room('Tue 14-16', '31/449')))).
+lecture_db(lecture(
+  'Tutorials Artificial Intelligence',
+  and('Gust', 'Kühnberger'),
+  and(time_room('Tue 16-18', '69/118'),
+    and(time_room('Wed 12-14', '69/E15'),
+      and(time_room('Thu 10-12', '32/372'),
+        time_room('Fri 12-14', '32/107')))))).
+
+
+/*--------------------------------------------------------------
 
 Define access predicates for all fields: lecture_name, teacher, time_room
 example: 
 
 lecture_name(lecture(X,_,_), X).
 
-for fields with multible values the access predicates must
+for fields with multiple values, the access predicates must
 decompose these values such that all the values can be
 enumerated, eg.
 
@@ -73,7 +111,19 @@ TR = time_room(t1,r1) ;
 TR = time_room(t2,r2) ;
 TR = time_room(t3,r3) .
 
---------------------------------------------------------------
+--------------------------------------------------------------*/
+
+lecture_name(lecture(N,_,_), N).
+
+teacher(lecture(_,P,_), P) :- atomic(P).
+teacher(lecture(_,and(P,_),_), P).
+teacher(lecture(N,and(_,X),TR), P) :- teacher(lecture(N,X,TR), P).
+
+time_room(lecture(_,_,time_room(T,R)), time_room(T, R)).
+time_room(lecture(_,_,and(time_room(T,R),_)), time_room(T,R)).
+time_room(lecture(N,P,and(_,X)), TR) :- time_room(lecture(N,P,X), TR).
+
+/*--------------------------------------------------------------
 
 Define a predicate 
 conflict(TimeRoom, Lecture1, Lecture2) :- ....
@@ -81,9 +131,18 @@ conflict(TimeRoom, Lecture1, Lecture2) :- ....
 which checks for a conflict (two different lectures are
 at the same time in the same room) and gives the time
 and room with the name of the two lectures. (Just compare
-the represenations of time slots like 'Thu 12-14')
+the representations of time slots like 'Thu 12-14')
 
---------------------------------------------------------------
+--------------------------------------------------------------*/
+
+conflict(TR, L1, L2) :-
+  lecture_db(L1),
+  lecture_db(L2),
+  time_room(L1, TR),
+  time_room(L2, TR),
+  L1 \= L2. % necessary because a lecture cannot conflict with itself
+
+/*--------------------------------------------------------------
 
 Define a predicate
 free_room(Time, Room) :- ....
@@ -97,76 +156,16 @@ and all possible rooms (again only those occurring in one the lectures).
 
 --------------------------------------------------------------*/
 
-lecture_db(lecture(
-  'Multi-Agent Systems', 
-  'Kühnberger', 
-  time_room('Thu 16-18','31/147'))).
+time(T) :-
+  lecture_db(L),
+  time_room(L, time_room(T,_)).
 
-lecture_db(lecture(
-  'Functional Programming', 
-  'Gust', 
-  time_room('Thu 12-14','69/E23'))).
+room(R) :-
+  lecture_db(L),
+  time_room(L, time_room(_,R)).
 
-lecture_db(lecture(
-  'Aspects of knowledge representation in artificial general intelligence', 
-  'Kühnberger', 
-  time_room('Thu 16-18','31/147'))).
-
-lecture_db(lecture(
-  'Entscheidungs- und Spieltheorie für Studierende der Kognitionswissenschaft', 
-  'Gaertner', 
-  time_room('Thu 14-16','69/E23'))).
-
-lecture_db(lecture(
-  'Modal Logic',
-  'Martinez Baldares',
-  time_room('Tue 14-16', '31/449'))).
-
-lecture_db(lecture(
-  'Introduction to Algebraic Logic',
-  and('Martinez Baldares',  'Kühnberger'),
-  time_room('Mon 14-16', '31/423'))).
-
-lecture_db(lecture(
-  'Artificial Intelligence',
-  and('Gust', 'Kühnberger'),
-  and(time_room('Mon 14-16, 31/449'), time_room('Tue 14-16', '31/449')))).
-
-lecture_db(lecture(
-  'Tutorials Artificial Intelligence',
-  and('Gust', 'Kühnberger'), 
-  and(time_room('Tue 16-18', '69/118'), 
-    and(time_room('Wed 12-14', '69/E15'), 
-      and(time_room('Thu 10-12', '32/372'), 
-        time_room('Fri 12:00-14:00', '32/107') ) ) ) ) ).
-
-lecture_db(lecture(
-  'testname',
-  and('testt1', and('testt2', and('testt3', 'testt4')) ), 
-  and(time_room('Tue 16-18', '69/118'), 
-    and(time_room('Wed 12-14', '69/E15'), 
-      and(time_room('Thu 10-12', '32/372'), 
-        time_room('Fri 12:00-14:00', '32/107') ) ) ) ) ).
-
-in_and(E, and(E, _RA)).
-in_and(E, and(_HA, and(RAH, RA))) :-
-  in_and(E, and(RAH, RA)).
-in_and(E, and(_HA, E)):-
-  E\=and(_X, _Y).
-
-lecture_name(lecture(N, T, TR), N)  :-
-  lecture_db(lecture(N, T, TR)).
-
-teacher(lecture(N, and(T, OT), TR), X) :- 
-  lecture_db(lecture(N, and(T, OT), TR)),
-  in_and(X, and(T, OT)).
-
-teacher(lecture(N, TR, TR), T) :-
-  lecture_db(lecture(N, T, TR)).
-
-time_room(lecture(N, T, and(TR, OTR)), X) :- 
-  lecture_db(lecture(N, T, and(TR, OTR))),
-  in_and(X, and(TR, OTR)).
-
-time_room(lecture(N, T, TR), TR) :-
-  lecture_db(lecture(N, T, TR)).
+free_room(T,R) :-
+  time(T),
+  room(R),
+  lecture_db(L),
+  not(time_room(L,time_room(T,R))).
