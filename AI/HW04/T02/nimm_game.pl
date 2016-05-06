ae87f932885131f987cbf723ed5a20bd124622ed
Kaitlin Hipkin <kah5368@rit.edu>
2016-05-06 19:26:13 +0200

    AI/HW04/T02 update nimm game move() predicate to only work for valid game states

--- AI/HW04/T02/nimm_game.pl
+++ AI/HW04/T02/nimm_game.pl
@@ -42,23 +42,26 @@ subst_element(E1, E2, L1, L2) :-
 	append(A,[E2|B],L2).
 
 %%
-% move(?S1, ?S2) - determines if S1 can be turned into S2 through a valid move 
-% in Nimmspiel
+% valid_nim_heap(?H) - true if the heap is a list of zero or more 'i's.
 %
-% ?S1 - the starting board state (i.e. the current state of the heaps)
-% ?S2 - the ending board state after one valid board move
+% ?H - the list
 %%
-
-% Recursive calls - For every heap that is on the board, we generate all 
-% possible 'sub-heaps' (i.e. we remove one element from one heap, then another)
-% and call those the possible moves
-move(S1, S2) :-
-    % L1 is all the possible sub-lists of S1, and L2 is the sub-lists of S2
-	subst_element(L1, L2, S1, S2),
-	take_from(L1, L2).
+valid_nim_heap([]).
+valid_nim_heap([i|R]) :-
+	valid_nim_heap(R).
 
 %%
-% take_from(+R1, ?R2) - given a list, produces another list that has one or 
+% valid_nim_state(?S) - true if the state is a list of zero or more valid heaps.
+%
+% ?S - the list
+%%
+valid_nim_state([]).
+valid_nim_state([H|T]) :-
+	valid_nim_heap(H),
+	valid_nim_state(T).
+
+%%
+% take_from(+R1, ?R2) - given a list, produces another list that has one or
 % more 'i' elements removed from the original list; This predicate is a helper
 % for the 'move' predicate
 %
@@ -66,10 +69,30 @@ move(S1, S2) :-
 % ?R2 - the new, smaller list (i.e. [i, i] OR [i] OR [])
 %%
 
-% Base Case: We remove one 'i' element and set R2 as the resulting list
+% Base Case: R1 is one 'i' element shorter than R2
 take_from([i|R], R).
 
-% Recursive Case: While R1 still has at least one element, keep removing 
-% elements
+% Recursive Case: R1 is more than one 'i' element shorter than R2
 take_from([i|R1], R2) :-
 	take_from(R1, R2).
+
+%%
+% move(?S1, ?S2) - true if S1 can be turned into S2 through a valid move
+% in Nimmspiel
+%
+% ?S1 - the starting board state (i.e. the current state of the heaps)
+% ?S2 - the ending board state after one valid board move
+%%
+
+% Recursive calls - For every heap that is on the board, we generate all
+% possible 'sub-heaps' (i.e. remove one or more elements from that heap)
+% and substitute each sub-heap for the original heap to generate
+% all possible moves
+move(S1, S2) :-
+	% L1 is a heap in S1, and L2 is a heap in the same position of S2
+	subst_element(L1, L2, S1, S2),
+	% L2 is some sub-heap of L1
+	take_from(L1, L2),
+	% both S2 and S2 must be valid Nim states
+	valid_nim_state(S1),
+	valid_nim_state(S2).
