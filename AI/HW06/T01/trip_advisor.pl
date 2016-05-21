6addbae79ca3ea3ca792416aa608406b71321949
Wilgy <tjw8700@rit.edu>
2016-05-21 20:10:47 +0200

    AI/HW06 replaced T01 with Brennan's implementation;

--- AI/HW06/T01/trip_advisor.pl
+++ AI/HW06/T01/trip_advisor.pl
@@ -23,10 +23,10 @@ poi(meppen, townhall).
 %
 % A city is worth a visit if it has a castle
 %
-visit(City, has_castle) :- poi(City, castle), !.
+visit(City, has_castle) :- poi(City, castle).
 
 % Bramsche is worth a visit for its own rights (Task 1)
-visit(bramsche, is_bramsche) :- !.
+visit(bramsche, is_bramsche).
 
 % A city is worth a visit
 % if it has two points of interest
@@ -34,4 +34,38 @@ visit(bramsche, is_bramsche) :- !.
 visit(City, has_2_poi(X,Y)) :-
     poi(City, X),
     poi(City, Y),
-    X \= Y, !.
+    !,
+    X \= Y.
+
+% Tests:
+% ?- visit(C, R).
+% C = osnabrueck,
+% R = has_castle ;
+% C = muenster,
+% R = has_castle ;
+% C = hannover,
+% R = has_castle ;
+% C = fuerstenau,
+% R = has_castle ;
+% C = bramsche,
+% R = is_bramsche ;
+% false.
+%
+% ?- visit(osnabrueck, _).
+% true.
+%
+% ?- visit(muenster, _).
+% true.
+%
+% ?- visit(hannover, _).
+% true.
+%
+% ?- visit(fuerstenau, _).
+% true.
+%
+% ?- visit(bramsche, _).
+% true.
+%
+% ?- visit(meppen, _).
+% false.
+
