159ad19ad2c3bee9093cc47db63808a1dfb2fa65
Wilgy <tjw8700@rit.edu>
2016-05-21 20:51:21 +0200

    AI/HW06 Added comments to T02 and T03, should be ready for submission;

--- AI/HW06/T02/and_or.pl
+++ AI/HW06/T02/and_or.pl
@@ -10,13 +10,46 @@
 %% so that they can be used for prolog queries and clauses.
 %% Parentheses for overriding precedence should work!
 
-%% Example:
+
+% Operation definitions for the boolean operators
+%-------------------------------------------------------------------------------
+% The NOT operator has the lowest precedence;  The functor comes before the 
+% variable/expression being evaluated
 :- op(100, fx, ~).
+
+% The AND operator has higher precedence than NOT, but less than OR; The 
+% expressions being evaluated are on the left and right of the functor
 :- op(200, xfx, &&).
+
+% The OR operator has the highest precedence; The expressions being evaluated 
+% are on the left and right of the functor
 :- op(300, xfx, v).
+%-------------------------------------------------------------------------------
 
+
+% Operator declarations (what the operators actually do)
+%-------------------------------------------------------------------------------
+%%
+% ~ +X - predicate will evaluate to true if the expression being evaluated 
+% cannot be proven true
+%
+% +X - the expression being 'negated'
+%%
 ~ X:- not(X).
-
+%%
+% +X1 && +X2 - predicate will evaluate to true when both X1 and X2 are true
+%
+% +X1 - the first (left-side) expression being evaluated
+% +X2 - the second (right-side) expression being evaluated
+%%
 X1 && X2 :- X1, X2.
 
+%%
+% +X1 v +X2 - predicate will evaluate to true when either X1 is true or X2 is 
+% true (or both are true)
+%
+% +X1 - the first (left-side) expression being evaluated
+% +X2 - the second (right-side) expression being evaluated
+%%
 X1 v X2 :- X1; X2.
+%-------------------------------------------------------------------------------
--- AI/HW06/T03/operators.pl
+++ AI/HW06/T03/operators.pl
@@ -33,47 +33,120 @@ gives the answer:
 Who = she
 */
 
+% Operator definitions to help define the 'syntax' of the pseudo-nl-sentences
+
+%-------------------------------------------------------------------------------
+% DATABASE MANIPULATION AND VIEWING
+%-------------------------------------------------------------------------------
+
+%%
+% The '??' operator 'queries' has the highest precedence of our 'language';
+%
+% The expression being evaluated appears on the left of the functor
+%%
 :- op(700, yf, ??).
+
+%%
+% The '#' operator h
+% Has the highest precedence in our 'language' (equal to the ??
+% operator, meaning that # and ?? cannot be used at the same time); 
+%
+% The expression being evaluated appears on the right of the functor
+%%
 :- op(700, fy, #).
+%-------------------------------------------------------------------------------
+% PSEUDO-NL-SENTENCES STRUCTURE DEFINITIONS
+%-------------------------------------------------------------------------------
 
-
+%%
+% The 'her' functor has less precedence than 'visited'; 
+%
+% The expression appears  at the right of the functor (i.e. 'brother')
+%%
 :- op(450, fx, her).
+
+%%
+% The 'visited' functor 
+% Has higher precedence than 'her'; 
+%
+% The expression on the right is the person doing the visiting (i.e. 'she'), 
+% the expression on the right is the person being visited (i.e. 'her brother');
+%%
 :- op(500, xfy, visited).
 
-:- op(600, xfx, where).
+%-------------------------------------------------------------------------------
 
+%%
+% The 'where' functor 
+% Has higher precedence than both 'visited' and 'traveled'; 
+%
+% The expression on the left is the 'traveled' fact being added and 
+% the expression on the right is the 'visited' fact being added
+%%
+:- op(600, xfx, where). 
+%-------------------------------------------------------------------------------
+
+%%
+% The 'from' functor 
+% Has lower precedence than 'to'; 
+%
+% The expression is on the the right of the functor (i.e. 'berlin')
+%%
 :- op(450, fx, from).
-:- op(475, xfx, to).
-:- op(500, xfy, traveled).
 
+%%
+% The 'to' operator
+% Has higher precedence than 'from', but lower precedence than 'traveled'
+%
+% The expression on the left original location (i.e 'from berlin') and 
+% the expression on the right is the destination (i.e. 'hamburg') 
+%%
+:- op(475, xfx, to).
+
+%%
+% The 'traveled' operator
+% Has higher precedence than 'to' and  'from'
+%
+% The expression on the right is the person traveling (i.e. 'mary') and 
+% The expression on the left is the travel plan (i.e. 'from berlin to hamburg')
+%%
+:- op(500, xfy, traveled).
+%-------------------------------------------------------------------------------
+
+% Operator declarations (what the operators actually do);
+% NOTE: The operators for our pseudo-nl-sentences do not need to be declared, 
+% since they do not do anything other than define a structure that our 'facts' 
+% database has (excluding 'where', which breaks up the facts)
+%-------------------------------------------------------------------------------
+%%
+% +X where +Y - takes two facts and asserts them into our database, then 
+% displays the new facts to the user, separated by new lines;
+%
+% +X - the first fact being asserted (i.e. 'traveled(...)')
+% +Y - the second fact being asserted (i.e. 'visited(...)')
+%%
 X where Y :- 
     assert(facts(X)), 
     assert(facts(Y)), 
     display(X), nl, 
     display(Y), nl.
 
+%%
+% # +X where +Y - 'helper' function to assert two new facts
+% 
+% +X - the first fact being asserted (i.e. 'traveled(...)')
+% +Y - the second fact being asserted (i.e. 'visited(...)')
+%%
 # X where Y :- where(X, Y).
 
+%%
+% +Action ?? - displays information about a fact that is already in our 
+% database
+%
+% +Action - the fact being queried (i.e. 'Who visited her brother ??')
+%%
 Action ?? :- facts(Action).
-
-%% SomeOne traveled SomeWhere :- 
-%%     SomeOne, 
-%%     SomeWhere.
-
-%% FromSomeWhere to SomePlace :- 
-%%     FromSomeWhere, 
-%%     SomePlace.
-
-%% from SomePlace :- SomePlace.
-
-
-%% SomeOne visited SomeOneElse :- 
-%%     SomeOne, 
-%%     SomeOneElse.
-
-%% her Relative :- Relative.
+%-------------------------------------------------------------------------------
 
 % this line will be processed as if it were entered as a query:
 :- # mary traveled from berlin to hamburg where she visited her brother.
-
-% now the above queries should be processed correctly
