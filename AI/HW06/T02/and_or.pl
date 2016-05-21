87094bd79d40f22a7fc1f85393c23f81a25b65f8
Kaitlin Hipkin <kah5368@rit.edu>
2016-05-21 16:31:39 +0200

    AI/HW06/T02  implement ops, add detailed test queries

--- AI/HW06/T02/and_or.pl
+++ AI/HW06/T02/and_or.pl
@@ -14,3 +14,36 @@
 
 %% visit(City) :-
 %%		(poi(City,beach) v poi(City,mountain)) && ~poi(City, nuclear_power_plant).
+
+% ~ operator: (for 'not')
+%		low precedence
+%		prefix operator type
+:- op(400,fx,~).
+~ X :- not(X). % map ~ to prolog's not operator
+
+% && operator: (for 'and')
+%		medium precedence
+%		left associative operator type
+:- op(500,yfx,&&).
+X && Y :- X, Y. % true if X and Y are true
+
+% v operator: (for 'or')
+%		high precedence
+%		left associative operator type
+:- op(600,yfx,v).
+X v Y :- X ; Y. % true if X is true, otherwise if Y is true
+
+/*
+Test queries:
+
+	true && true && true && false. // false
+	false v false v false v true. // true
+	~ true. // false
+	~ false. // true
+	~ true && false. // false
+	~ (true && false). // true
+
+	display(x v y && ~ z). // v(x, &&(y, ~(z)))
+	display(~ x && y v ~ z). // v(&&(~(x), y), ~(z))
+	display(~ (x && y) v ~ z). // v(~(&&(x, y)), ~(z))
+*/
