e39b419722b868f02fc3028cce60d620cfd6138b
Kaitlin Hipkin <kah5368@rit.edu>
2016-05-29 18:27:05 +0200

    AI/HW07/T01 PartB implement improved approach

--- AI/HW07/T01/cryptarithm1.pl
+++ AI/HW07/T01/cryptarithm1.pl
@@ -46,8 +46,7 @@ generate(L) :-
 
 select_unique([], []).
 select_unique([L|LRest], Digits) :-
-    select(D, Digits, DRest),
-    L = D,
+    select(L, Digits, DRest),
     select_unique(LRest, DRest).
 
 %                           ( M*100 + A*10 + N )
@@ -80,4 +79,41 @@ man_bit_monkey([M, A, N], [B, I, T], [M, O, N, K, E ,Y]) :-
 % Y = 4 ;
 %
 % % 43,115,730 inferences, 7.784 CPU in 7.800 seconds (100% CPU, 5538700 Lips)
+% false.
+
+%-------------------------------------------------------------------------------
+% PART B
+
+generate_i([M, A, N, B, I, T, O, K, E, Y]) :-
+	select_unique_i([M, B], [1, 2, 3, 4, 5, 6, 7, 8, 9], L),
+	append([0], L, L1),
+	select_unique_i([A, N, I, T, O, K, E, Y], L1, []).
+
+select_unique_i([], L, L).
+select_unique_i([L|LRest], Digits, Rem) :-
+    select(L, Digits, DRest),
+    select_unique_i(LRest, DRest, Rem).
+
+test_i([M, A, N, B, I, T, O, K, E, Y]) :-
+    0 is ((M*100000) + (O*10000) + (N*1000) + (K*100) + (E*10) + Y) -
+		(((M*100) + (A*10) + N) * ((B*100) + (I*10) + T)).
+
+man_bit_monkey_i([M, A, N], [B, I, T], [M, O, N, K, E ,Y]) :-
+	generate_i([M, A, N, B, I, T, O, K, E, Y]),
+	test_i([M, A, N, B, I, T, O, K, E, Y]).
+
+% ?- time(man_bit_monkey_i([M, A, N], [B, I, T], [M, O, N, K, E ,Y])).
+% 1,983,856 inferences, 0.499 CPU in 0.495 seconds (101% CPU, 3974045 Lips)
+% M = 1,
+% A = 3,
+% N = 9,
+% B = 7,
+% I = 8,
+% T = 6,
+% O = 0,
+% K = 2,
+% E = 5,
+% Y = 4 ;
+%
+% 24,593,249 inferences, 5.944 CPU in 5.959 seconds (100% CPU, 4137743 Lips)
 % false.
